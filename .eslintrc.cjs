d364f8ab050372f42041391c0667b20b9aaaf0e7
DESKTOP-D5V7SAA\Inna <iforkert@leomax.de>
2026-01-28 13:07:45 +0200

    chore: initial project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  }
+}
