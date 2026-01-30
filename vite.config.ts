9b2b187bc3fc463da4a8915bb16d235667b87079
DESKTOP-D5V7SAA\Inna <iforkert@leomax.de>
2026-01-30 15:37:58 +0200

    fix: base url for local setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import vue from "@vitejs/plugin-vue"
 import { fileURLToPath } from "url"
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [vue()],
-  base: "/eliza-chat/",
+  base: command === "build" ? "/eliza-chat/" : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   }
-})
+}))
